11f53c419650b5e1d2ba98cebad985a09b2fe958
AnujMishra-0 <er.anujmishra0@gmail.com>
2025-06-22 16:14:23 +0530

    uploaded "Design principles & Patterns" mandatory hands-on
    contains the code and output files of both "Singleton Pattern exercises" and "Factory Method Pattern Exercises".

---
+++ Week 1 Design principles & Patterns/Exercise 2: Implementing the Factory Method Pattern/FactoryMethodPatternExample/Documents/worddocument.java
@@ -0,0 +1,8 @@
+package Documents;
+
+public class worddocument implements Documents.Document {
+    @Override
+    public void open() {
+        System.out.println("Opening Excel Documents.Document...");
+    }
+}
